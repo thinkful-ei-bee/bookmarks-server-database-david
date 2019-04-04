b54fe42b47d3fd40100f7137c3d4f35b96cd5ad4
David Bolin <david@bolin.at>
2019-04-04 13:10:40 -0700

    starting to create bookmarks route

---
+++ src/bookmarks/bookmarks.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const store = require('../store');
+
+router.route('/bookmarks')
+  .get((req, res) => {
+    res.json(store);
+  });
---
+++ src/store.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//contents for testing purposes
+const store = [
+  {
+    'id': 'dskokaa',
+    'title': 'Google',
+    'url': 'http://google.com',
+    'desc': 'Search engine',
+    'rating': 5
+  },
+  {
+    'id': '87fn36vd9djd',
+    'title': 'Github',
+    'url': 'https://github.com',
+    'desc': 'a site for code collaboration',
+    'rating': 3
+  }
+];
+
+module.exports = store;
