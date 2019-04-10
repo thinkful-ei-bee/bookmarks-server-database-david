8e64a757a6260c93509bd9efda00b6a015a6f32f
David Bolin <david@bolin.at>
2019-04-10 12:56:03 -0700

    created fake seed data

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,15 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('Title 1', 'http://www.nonexistentsite.com', 'Lorem ipsum dolor etc', 3),
+  ('Title 2', 'http://www.nonexistentsite32.com', 'Lorem ipsum dolor etc', 1),
+  ('Better Title 1', 'http://www.nonexistentsite131.com', 'Lorem ipsum dolor etc', 5),
+  ('Better Title 2', 'http://www.nonexistentsite2.com', 'Lorem ipsum dolor etc', 5),
+  ('Title 5', 'http://www.doesnotexist.com', 'Lorem ipsum dolor etc', 2),
+  ('Title 6', 'http://www.nonexistentsite155.com', 'Lorem ipsum dolor etc', 3),
+  ('Title 7', 'http://www.nonexistentsite201.com', 'Lorem ipsum dolor etc', 3),
+  ('Title 8', 'http://www.nonexistentsite300.com', 'Lorem ipsum dolor etc', 1),
+  ('Title 9', 'http://www.nonexistentsite1000.com', 'Lorem ipsum dolor etc', 1),
+  ('Title 10', 'http://www.nonexistent.com', 'Lorem ipsum dolor etc', 4),
+  ('Title 11', 'http://www.nonexistent2.com', 'Lorem ipsum dolor etc', 3),
+  ('Last Title', 'http://www.nonexistent3.com', 'Lorem ipsum dolor etc', 5)
+  
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,12 @@
 'use strict';
+require('dotenv').config();
 /* global supertest */
 const app = require('../src/app');
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / without authorization responds with 401', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, world!');
+      .expect(401);
   });
 });
