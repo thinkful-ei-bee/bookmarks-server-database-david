e622963ba38a7c4204a3cd5a9eef23e7c58af239
David Bolin <david@bolin.at>
2019-04-14 14:09:52 -0700

    changed route for posting bookmarks for consistency

--- src/bookmarks/bookmarks.js
+++ src/bookmarks/bookmarks.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const bodyParser = express.json();
+const logger = require('../logger');
 const bookmarkService = require('./bookmarkService');
 
 router.route('/bookmarks')
@@ -10,6 +12,28 @@ router.route('/bookmarks')
     bookmarkService.getAllBookmarks(knex)
       .then(articles => {
         res.json(articles);
+      })
+      .catch(next);
+  })
+  .post(bodyParser, (req, res, next) => {
+    const {title, url, description, rating} = req.body;
+    const newBookmark = { title, url, description, rating };
+    const knex = req.app.get('db');
+    
+    if (url.length < 5 || (url.slice(0, 5) !== 'http:' && url.slice(0, 5) !== 'https')) {
+      logger.error('invalid url');
+      return res.status(400).send('url required with http or https protocol');
+    } 
+
+    if (rating < 1 || rating > 5) {
+      logger.error('invalid rating');
+      return res.status(400).send('rating should be from 1 to 5');
+    }
+    bookmarkService.insertMark(knex, newBookmark)
+      .then(item => {
+        res.status(201).json({
+          id: item.id
+        });
       })
       .catch(next);
   });
